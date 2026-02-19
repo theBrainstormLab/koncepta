f28e5483bd9664b7a9d68ff5124197c12079c70d
shaaanuu <monshan50@gmail.com>
2026-02-19 06:50:40 +0530

    add: rough hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Icon } from "@iconify-icon/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="home">
+        <div className="nav">
+          <a href="/" className="nav-item">
+            koncepta
+          </a>
+          <a href="/" className="nav-item">
+            about
+          </a>
+          <div className="nav-icon">
+            <Icon icon="ri:sun-line" width="22" height="22" />
+          </div>
+          <a href="/" className="nav-item">
+            notes
+          </a>
+          <a href="/" className="nav-item">
+            syllabus
+          </a>
+        </div>
+
+        <div className="title">
+          <h1>notes</h1>
+          <h1>concepts</h1>
+          <h1>ready</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
