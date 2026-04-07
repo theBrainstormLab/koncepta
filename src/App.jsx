fda21badde7e1d45b5e72630637d287fb6e27a3b
itsmethaj <itsmethaj@email.com>
2026-04-08 01:41:59 +0530

    feat: 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { Routes, Route } from 'react-router-dom'
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
-
+import NotFound from './pages/NotFound';
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
+        {/* <Route path="/about" element={<About />} /> */}   
+        <Route path="*" element={<NotFound />} />
+         </Routes>  
       <Footer />
     </>
 
