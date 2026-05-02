0f46103ac66cf8035bf9ec455785fec337ad26a9
shaaanuu <monshan50@gmail.com>
2026-05-02 21:06:27 +0530

    refactor: replace hardcoded colors with CSS variables in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,14 @@
+import Cat from "../../public/cat.svg?react";
+
 export default function Footer() {
   return (
-    <div className="flex flex-col justify-center items-center text-[#b0b0b0] mt-auto relative py-5 gap-[10px] md:flex-row md:gap-0">
-      <img
-        src="./cat.svg"
-        alt="cat"
-        className="h-12 sm:h-16 md:h-24 md:absolute md:left-1/4"
-      />
+    <div className="flex flex-col justify-center items-center text-[var(--color-text-footer)] mt-auto relative py-5 gap-[10px] md:flex-row md:gap-0">
+      <Cat className="h-12 sm:h-16 md:h-24 md:absolute md:left-1/4" />
       <div className="relative text-center z-[1]">
         <a
           href="https://github.com/theBrainstormLab"
           target="_blank"
-          className="![font-family:Poppins-Bold] text-[10px] text-[#b0b0b0] sm:text-xs md:text-sm"
+          className="![font-family:Poppins-Bold] text-[10px] sm:text-xs md:text-sm"
         >
           © brainstormLab 2026
         </a>
